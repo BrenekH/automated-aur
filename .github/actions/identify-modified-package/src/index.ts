85e4d43f66d0d9b06ea566d9c092565d95614f14
Brenek Harrison <brenekharrison@gmail.com>
2021-07-16 13:08:04 -0600

    Patch URL

--- .github/actions/identify-modified-package/src/index.ts
+++ .github/actions/identify-modified-package/src/index.ts
@@ -1,8 +1,16 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
+import { PullRequest } from "@octokit/webhooks-definitions/schema";
 
 try {
-	console.log(github.context);
+
+	const pr = github.context.payload as PullRequest;
+	const octokit = github.getOctokit(core.getInput("token"));
+
+	console.log(pr.patch_url);
+
+	console.log(octokit.request(pr.patch_url));
+
 } catch (error) {
 	core.setFailed(error);
 }
