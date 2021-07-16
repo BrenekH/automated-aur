f1955262a5958f962dd441d8cd0b553984b62989
Brenek Harrison <brenekharrison@gmail.com>
2021-07-16 13:23:10 -0600

    We need to go deeper

--- .github/actions/identify-modified-package/src/index.ts
+++ .github/actions/identify-modified-package/src/index.ts
@@ -9,28 +9,28 @@ try {
 		throw "Not a pull request";
 	}
 
-	const pr = context.payload as PullRequest;
+	const pr = context.payload.pull_request as PullRequest;
 	const octokit = github.getOctokit(core.getInput("token"));
 
-	octokit.auth;
+	// octokit.auth;
 
-	console.log(pr);
+	// console.log(pr);
 
-	// console.log(pr.diff_url);
+	console.log(pr.diff_url);
 
-	// octokit.request(pr.diff_url).then((value) => {
-	// 	console.log(value);
-	// }).catch((error) => {
-	// 	core.setFailed(error);
-	// });
+	octokit.request(pr.diff_url).then((value) => {
+		console.log(value);
+	}).catch((error) => {
+		core.setFailed(error);
+	});
 
-	// console.log(pr.patch_url);
+	console.log(pr.patch_url);
 
-	// octokit.request(pr.patch_url).then((value) => {
-	// 	console.log(value);
-	// }).catch((error) => {
-	// 	core.setFailed(error);
-	// });
+	octokit.request(pr.patch_url).then((value) => {
+		console.log(value);
+	}).catch((error) => {
+		core.setFailed(error);
+	});
 
 } catch (error) {
 	core.setFailed(error);
