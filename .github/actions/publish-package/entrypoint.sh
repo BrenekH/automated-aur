369cd59fbb5817b3e6fb821c81a836197a8eee25
Brenek Harrison <brenekharrison@gmail.com>
2021-07-16 08:39:21 -0600

    Hopefully allow the creation of the ssh key

--- .github/actions/publish-package/entrypoint.sh
+++ .github/actions/publish-package/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-mkdir -p /home/builduser/.ssh
-chmod 700 /home/builduser/.ssh
-echo "aur.archlinux.org ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEuBKrPzbawxA/k2g6NcyV5jmqwJ2s+zpgZGZ7tpLIcN" > known_hosts
-echo "$2" > /home/builduser/.ssh/aur_key
-chmod 400 /home/builduser/.ssh/aur_key
-export GIT_SSH_COMMAND="ssh -i /home/builduser/.ssh/aur_key"
+sudo mkdir -p /home/builder
+sudo chown -R /home/builder
+mkdir -p /home/builder/.ssh
+chmod 700 /home/builder/.ssh
+echo "aur.archlinux.org ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEuBKrPzbawxA/k2g6NcyV5jmqwJ2s+zpgZGZ7tpLIcN" > /home/builder/.ssh/known_hosts
+echo "$2" > /home/builder/.ssh/aur_key
+chmod 600 /home/builder/.ssh/aur_key
+export GIT_SSH_COMMAND="ssh -i /home/builder/.ssh/aur_key"
 
 /publishpkg.py "$1"
