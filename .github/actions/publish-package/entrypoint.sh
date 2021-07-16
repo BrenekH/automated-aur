8d77191f3a706b3172d8711790f60234468b4de7
Brenek Harrison <brenekharrison@gmail.com>
2021-07-16 08:33:45 -0600

    Install ssh key

--- .github/actions/publish-package/entrypoint.sh
+++ .github/actions/publish-package/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-/publishpkg.py "$@"
+mkdir -p /home/builduser/.ssh
+chmod 700 /home/builduser/.ssh
+echo "aur.archlinux.org ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEuBKrPzbawxA/k2g6NcyV5jmqwJ2s+zpgZGZ7tpLIcN" > known_hosts
+echo "$2" > /home/builduser/.ssh/aur_key
+chmod 400 /home/builduser/.ssh/aur_key
+export GIT_SSH_COMMAND="ssh -i /home/builduser/.ssh/aur_key"
+
+/publishpkg.py "$1"
