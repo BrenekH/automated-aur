6eb79f21c0f920f10d3c320ad3a742f63d67f6f4
Brenek Harrison <brenekharrison@gmail.com>
2021-07-16 13:46:20 -0600

    Maybe list files will do what I want

--- .github/actions/identify-modified-package/src/index.ts
+++ .github/actions/identify-modified-package/src/index.ts
@@ -1,6 +1,6 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
-import { PullRequest } from "@octokit/webhooks-definitions/schema";
+// import { PullRequest } from "@octokit/webhooks-definitions/schema";
 
 try {
 
@@ -9,24 +9,13 @@ try {
 		throw "Not a pull request";
 	}
 
-	const pr = context.payload.pull_request as PullRequest;
+	// const pr = context.payload.pull_request as PullRequest;
 	const octokit = github.getOctokit(core.getInput("token"));
 
-	// octokit.auth;
-
-	// console.log(pr);
-
-	console.log(pr.diff_url.replace("https://github.com", ""));
-
-	octokit.request(pr.diff_url.replace("https://github.com", ""), {headers: {Accept: "application/vnd.github.v3.diff"}}).then((value) => {
-		console.log(value);
-	}).catch((error) => {
-		core.setFailed(error);
-	});
-
-	console.log(pr.patch_url.replace("https://github.com", ""));
-
-	octokit.request(pr.patch_url.replace("https://github.com", ""), {headers: {Accept: "application/vnd.github.v3.patch"}}).then((value) => {
+	octokit.rest.pulls.listFiles({
+		...context.repo,
+		pull_number: context.payload.pull_request?.number as number,
+	}).then((value) => {
 		console.log(value);
 	}).catch((error) => {
 		core.setFailed(error);
