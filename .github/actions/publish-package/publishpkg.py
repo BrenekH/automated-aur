9080432c8b0190daf3b1bd9dc714258a4e19e1dd
Brenek Harrison <brenekharrison@gmail.com>
2021-07-15 23:22:39 -0600

    Clone AUR repo and create .SRCINFO

--- .github/actions/publish-package/publishpkg.py
+++ .github/actions/publish-package/publishpkg.py
@@ -1,15 +1,40 @@
 #!/bin/env python3
-import sys
+import json, shutil, subprocess, sys, tempfile
+from pathlib import Path
+from typing import List
 
-def main():
-	# TODO: Clone the existing AUR repo to a dir in `/tmp`.
-	# TODO: Copy `PKGBUILD` and everything in the `manifest.include` array to the repo.
-	# TODO: Recreate `.SRCINFO` using `makepkg --printsrcinfo > .SRCINFO`.
-	# TODO: Ensure proper `.gitignore` file is in the repo (useful for new packages, not yet uploaded).
-	# TODO: Force-add all modified files to the repo
-	# TODO: Push to AUR
-	# TODO: Output any complications to standard output and exit with the proper code.
+def main(_package_dir: str):
+	pkg_dir = Path(_package_dir)
+
+	with (pkg_dir / ".aurmanifest.json").open("r") as f:
+		manifest = json.load(f)
+
+	with tempfile.TemporaryDirectory() as git_td:
+		# Clone the existing AUR repo to a dir in `/tmp`.
+		subprocess.check_call(["git", "clone", f"https://aur.archlinux.org/{manifest['name']}.git", git_td])
+
+		# Copy `PKGBUILD` and everything in the `manifest.include` array to the repo.
+		copy_files_to_dir([pkg_dir / "PKGBUILD"] + [pkg_dir / f for f in manifest["include"]], Path(git_td))
+
+		# Recreate `.SRCINFO` using `makepkg --printsrcinfo > .SRCINFO`.
+		subprocess.check_call(["makepkg", "--printsrcinfo", ">", ".SRCINFO"], shell=True)
+
+		# TODO: Ensure proper `.gitignore` file is in the repo (useful for new packages, not yet uploaded).
+
+		# TODO: Force-add all modified files to the repo
+
+		# TODO: Push to AUR
+
+		# TODO: Output any complications to standard output and exit with the proper code.
+
 	return
+
+def copy_files_to_dir(files: List[Path], dir: Path):
+	for f in files:
+		if f.is_absolute():
+			print(f"{f} is an absolute Path. It will not be copied.")
+			continue
+		shutil.copy(f, dir / f.name)
 
 if __name__ == "__main__":
 	main(sys.argv[1])
