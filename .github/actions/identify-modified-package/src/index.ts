320209f676a787877da1b6a06044d9dc6a1d4c5f
Brenek Harrison <brenekharrison@gmail.com>
2021-07-19 00:41:59 -0600

    Don't push if already pushed

--- .github/actions/identify-modified-package/src/index.ts
+++ .github/actions/identify-modified-package/src/index.ts
@@ -21,7 +21,8 @@ try {
 	resp.data.forEach((file) => {
 		const splitFName = file.filename.split("/");
 		if (splitFName[0] === "pkgs") {
-			moddedPackages.push(`${splitFName[0]}/${splitFName[1]}`)
+			const splicedString = `${splitFName[0]}/${splitFName[1]}`;
+			if (moddedPackages.indexOf(splicedString) === -1) moddedPackages.push(splicedString)
 		}
 	});
 
