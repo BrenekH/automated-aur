77946d191a24814bbce0b884976e24bedf67f869
Brenek Harrison <brenekharrison@gmail.com>
2021-07-24 19:55:57 -0600

    Use glob and glob-promise to identify packages

--- .github/actions/update-package-via-manifest/src/index.ts
+++ .github/actions/update-package-via-manifest/src/index.ts
@@ -1,6 +1,16 @@
 import * as core from "@actions/core";
+import { promise as glob } from "glob-promise";
 
 try {
+	// Identify all packages in the pkgs directory
+	const manifests = await glob("pkgs/**/.aurmanifest.json")
+
+	manifests.forEach((manifestPath) => {
+		core.info(manifestPath)
+
+		// TODO: Run a check for updates according to .aurmanifest.json
+		// TODO: Open a PR with updates
+	})
 
 } catch (error) {
 	core.setFailed(error);
